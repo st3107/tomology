fc2804df054d919aef1677ddcb1d5e38baedab80
st3107 <st3107@columbia.edu>
2021-07-14 15:03:19 -0400

    DEV: finish the development of the plans

--- tomography/tests/test_plans.py
+++ tomography/tests/test_plans.py
@@ -14,9 +14,48 @@ def test_fly_scan_2d():
     motor_x = sim.DelayedSynAxis(name="motor_x")
     motor_x.configure({"velocity": 1.})
 
-    plan = plans.fly_scan_2d(
-        area_det, [], shutter, motor_x, 0., 2., 3, motor_y, 0., 1., 2,
-        dwell_time=1., time_per_frame=1., move_velocity=1., shutter_close=1, shutter_open=0., shutter_wait=0.
+    plan = plans.fly_scan_nd(
+        [area_det], motor_y, 0., 1., 2, motor_x, 0., 2., 3,
+        time_per_point=1., time_per_frame=1., move_velocity=1., shutter=shutter, shutter_close=1, shutter_open=0.,
+        shutter_wait_close=0., shutter_wait_open=0.
+    )
+
+    summarize_plan(plan)
+
+
+def test_fly_scan_3d():
+    shutter = sim.SynSignal(name="shutter")
+    area_det = sim.FakeAreaDetector(name="dexela")
+    area_det.cam.configure({"acquire_time": 1.})
+    motor_y = sim.DelayedSynAxis(name="motor_y")
+    motor_y.configure({"velocity": 1.})
+    motor_z = sim.DelayedSynAxis(name="motor_z")
+    motor_z.configure({"velocity": 1.})
+    motor_x = sim.DelayedSynAxis(name="motor_x")
+    motor_x.configure({"velocity": 1.})
+
+    plan = plans.fly_scan_nd(
+        [area_det], motor_z, 2., 3., 2, motor_y, 1., 2., 2, motor_x, 0., 1., 2,
+        time_per_point=1., time_per_frame=1., move_velocity=1., shutter=shutter, shutter_close=1, shutter_open=0.,
+        shutter_wait_close=0., shutter_wait_open=0.
+    )
+
+    summarize_plan(plan)
+
+
+def test_grid_scan_2d():
+    shutter = sim.SynSignal(name="shutter")
+    area_det = sim.FakeAreaDetector(name="dexela")
+    area_det.cam.configure({"acquire_time": 1.})
+    motor_y = sim.DelayedSynAxis(name="motor_y")
+    motor_y.configure({"velocity": 1.})
+    motor_x = sim.DelayedSynAxis(name="motor_x")
+    motor_x.configure({"velocity": 1.})
+
+    plan = plans.grid_scan_nd(
+        [area_det], motor_y, 0., 1., 2, motor_x, 0., 2., 3,
+        time_per_point=1., time_per_frame=1., shutter=shutter, shutter_close=1., shutter_open=0.,
+        shutter_wait_close=0., shutter_wait_open=0.
     )
 
     summarize_plan(plan)
